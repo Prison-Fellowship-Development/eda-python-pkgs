0857166c4559f7b7e0a061c04f04baaa15702e79
szicari-streambit <80933567+szicari-streambit@users.noreply.github.com>
2025-06-18 22:52:47 -0500

    lib updates.

--- pfm/consumer.py
+++ pfm/consumer.py
@@ -1,26 +1,47 @@
 import os
+from contextlib import contextmanager
+
 import confluent_kafka
 
 
 class Consumer:
-
-    def __init__(self, topic, group_id = None):
+    def __init__(self, topic, group_id=None):
         self.topic = topic
         self.group_id = group_id
         if self.group_id is None:
-           self.group_id = 'consumer-group-' + str(os.getpid())
-        self.consumer = confluent_kafka.Consumer({
-            'bootstrap.servers': os.getenv('PFM_EVENT_SERVERS','localhost:19092,localhost:29092,localhost:39092'),
-            'security.protocol': os.getenv('PFM_EVENT_SECURITY_PROTOCOL','SASL_PLAINTEXT'),
-            'sasl.mechanism': os.getenv('PFM_EVENT_SASL_MECHANISM','SCRAM-SHA-256'),
-            'sasl.username': os.getenv('PFM_EVENT_SASL_USERNAME','superuser'),
-            'sasl.password': os.getenv('PFM_EVENT_SASL_PASSWORD','secretpassword'),
-            'group.id': self.group_id,
-            'auto.offset.reset': 'earliest'
-        })
-        self.consumer.subscribe([topic])
+            self.group_id = "consumer-group-" + str(
+                os.getpid()
+            )  # TODO: OS-agnostic default
+        self.consumer = None
 
-    def poll(self, timeout = 1.0):
+    @contextmanager
+    def consume(self):
+        if not self.consumer:
+            self.consumer = confluent_kafka.Consumer(
+                {
+                    "bootstrap.servers": os.getenv(
+                        "PFM_EVENT_SERVERS",
+                        "localhost:19092,localhost:29092,localhost:39092",
+                    ),
+                    "security.protocol": os.getenv(
+                        "PFM_EVENT_SECURITY_PROTOCOL", "SASL_PLAINTEXT"
+                    ),
+                    "sasl.mechanism": os.getenv(
+                        "PFM_EVENT_SASL_MECHANISM", "SCRAM-SHA-256"
+                    ),
+                    "sasl.username": os.getenv("PFM_EVENT_SASL_USERNAME", "superuser"),
+                    "sasl.password": os.getenv(
+                        "PFM_EVENT_SASL_PASSWORD", "secretpassword"
+                    ),
+                    "group.id": self.group_id,
+                    "auto.offset.reset": "earliest",
+                }
+            )
+            self.consumer.subscribe([self.topic])
+
+        yield
+
+    def poll(self, timeout=1.0):
         return self.consumer.poll(timeout)
 
     def close(self):

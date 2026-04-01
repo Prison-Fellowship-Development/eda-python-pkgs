a7ad8fbc2a2baf8323f3096b6aabbeaba3acc682
Mitch Martin <mitch_martin@pfm.org>
2026-04-01 15:24:38 -0500

    Fixing if condition

--- pfm/serializer.py
+++ pfm/serializer.py
@@ -32,7 +32,7 @@ class Serializer:
         self.avro_deserializer = None
 
         # if subject name was passed in, then setup schema and avro seralizers
-        if subject_name is not None:
+        if self.subject_name is not None:
             self.schema = self.schema_registry_client.get_latest_version(self.subject_name)
             self.format = self.schema.schema_type
             if self.format == 'AVRO':

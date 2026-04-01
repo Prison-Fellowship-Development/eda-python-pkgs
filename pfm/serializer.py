12dc7e21f5f95a03ce8e25bcbd9ceee0de91fdf7
Mitch Martin <mitch_martin@pfm.org>
2026-04-01 14:53:58 -0500

    Adding an auto decoder to the Serializer class

--- pfm/serializer.py
+++ pfm/serializer.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+from struct import unpack
+
 from confluent_kafka.serialization import SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient, record_subject_name_strategy
 from confluent_kafka.schema_registry.avro import AvroSerializer, AvroDeserializer
@@ -9,9 +11,8 @@ from confluent_kafka import avro
 
 class Serializer:
 
-    def __init__(self, subject_name, format = 'AVRO'):
-        self.subject_name = subject_name
-        self.format = format
+    def __init__(self, subject_name = None, format = None):
+        # setup schema registry client
         self.username_password = os.getenv('PFM_EVENT_SASL_USERNAME','superuser') + ':' + os.getenv('PFM_EVENT_SASL_PASSWORD','secretpassword')
         self.schema_registry_client = SchemaRegistryClient(
             conf = {
@@ -19,17 +20,32 @@ class Serializer:
                 'basic.auth.user.info': self.username_password
             }
         )
-        self.schema = self.schema_registry_client.get_latest_version(self.subject_name)
-        self.avro_serializer = AvroSerializer(
-            schema_registry_client = self.schema_registry_client,
-            schema_str = self.schema.schema.schema_str,
-            to_dict = self.json_to_dict
-        )
-        self.avro_deserializer = AvroDeserializer(
-            schema_registry_client = self.schema_registry_client,
-            schema_str = self.schema.schema.schema_str,
-            from_dict = self.dict_to_json
-        )
+
+        # set subject name and format
+        self.subject_name = subject_name
+        self.format = format
+        
+        # default class vars to None
+        self.schema = None
+        self.schema_id = None
+        self.avro_serializer = None
+        self.avro_deserializer = None
+
+        # if subject name was passed in, then setup schema and avro seralizers
+        if subject_name is not None:
+            self.schema = self.schema_registry_client.get_latest_version(self.subject_name)
+            self.format = self.schema.schema_type
+            if self.format == 'AVRO':
+                self.avro_serializer = AvroSerializer(
+                    schema_registry_client = self.schema_registry_client,
+                    schema_str = self.schema.schema.schema_str,
+                    to_dict = self.json_to_dict
+                )
+                self.avro_deserializer = AvroDeserializer(
+                    schema_registry_client = self.schema_registry_client,
+                    schema_str = self.schema.schema.schema_str,
+                    from_dict = self.dict_to_json
+                )
 
     def encode(self, topic, data):
         return self.avro_serializer(data, SerializationContext(topic, MessageField.VALUE))
@@ -45,3 +61,44 @@ class Serializer:
             return None
         tmp_str = json.dumps(obj)
         return json.loads(tmp_str)
+
+    def auto_decode(self, topic, data):
+        # try to determine schema using confluent's schema registry wire format
+
+        #+-------------+-------------------+
+        #| Byte Offset | Content           |
+        #+-------------+-------------------+
+        #| 0           | Magic byte (0x00) |
+        #| 1 – 4       | Schema ID (int32) |
+        #| 5 – end     | Avro/JSON payload |
+        #+-------------+-------------------+
+        prefix = data[:5]
+        magic, schema_id = unpack('>bI', prefix)
+
+        # if magic byte is not 0 or if schema id not > 0, then data must be a plain JSON string
+        if magic != 0 or schema_id <= 0:
+            return json.loads(data)
+        
+        # magic byte is 0 and schema id > 0, so lookup the schema and format and decode data
+        if self.schema_id != schema_id:
+            self.schema_id = schema_id
+            # get all subjects for the schema id
+            subjects = self.schema_registry_client.get_subjects_by_schema_id(self.schema_id)
+            self.subject_name = subjects[0]
+            # get the schema for the latest version of the subject
+            self.schema = self.schema_registry_client.get_latest_version(self.subject_name)
+            self.format = self.schema.schema_type
+            # if schema format is AVRO, setup the deserializer
+            if self.format == 'AVRO':
+                self.avro_deserializer = AvroDeserializer(
+                    schema_registry_client = self.schema_registry_client,
+                    schema_str = self.schema.schema.schema_str,
+                    from_dict = self.dict_to_json
+                )
+            else:
+                self.avro_deserializer = None
+
+        if self.format == 'AVRO':
+            return self.avro_deserializer(data, SerializationContext(topic, MessageField.VALUE))
+        else:
+            return json.loads(data)

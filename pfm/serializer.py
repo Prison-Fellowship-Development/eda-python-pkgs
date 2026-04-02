fed9929218703cfd4c2c3b21eaa1e96007970d18
Mitch Martin <mitch_martin@pfm.org>
2026-04-02 15:44:43 -0500

    Trying to force record_subject_name_strategy for AVRO serialization

--- pfm/serializer.py
+++ pfm/serializer.py
@@ -39,6 +39,10 @@ class Serializer:
                 self.avro_serializer = AvroSerializer(
                     schema_registry_client = self.schema_registry_client,
                     schema_str = self.schema.schema.schema_str,
+                    conf = {
+                        'auto.register.schemas': False,
+                        'subject.name.strategy': 'record_subject_name_strategy'
+                    },
                     to_dict = self.json_to_dict
                 )
                 self.avro_deserializer = AvroDeserializer(

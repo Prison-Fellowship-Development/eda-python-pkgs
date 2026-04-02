641013001c821075dd484ef8658b0b1e2cbbdf26
Mitch Martin <mitch_martin@pfm.org>
2026-04-02 09:55:07 -0500

    Adding Serializer methods to get subject name and schema and updating auto_decode method

--- pfm/serializer.py
+++ pfm/serializer.py
@@ -62,9 +62,8 @@ class Serializer:
         tmp_str = json.dumps(obj)
         return json.loads(tmp_str)
 
-    def auto_decode(self, topic, data):
+    def get_schema_id(self, data):
         # try to determine schema using confluent's schema registry wire format
-
         #+-------------+-------------------+
         #| Byte Offset | Content           |
         #+-------------+-------------------+
@@ -74,22 +73,35 @@ class Serializer:
         #+-------------+-------------------+
         prefix = data[:5]
         magic, schema_id = unpack('>bI', prefix)
+        if magic == 0 and schema_id > 0:
+            return schema_id
+        else:
+            return None
 
-        # if magic byte is not 0 or if schema id not > 0, then data must be a plain JSON string
-        if magic != 0 or schema_id <= 0:
+    def get_subject_name(self, schema_id):
+        # get all subjects for the schema id
+        subjects = self.schema_registry_client.get_subjects_by_schema_id(schema_id)
+        return subjects[0]
+
+    def get_schema(self, subject_name):
+        # get the schema for the latest version of the subject
+        return self.schema_registry_client.get_latest_version(subject_name)
+
+    def auto_decode(self, topic, data):
+        # try to get schema id from message payload (ie, data)
+        schema_id = self.get_schema_id(data)
+        if schema_id is None or schema_id == 0:
+            # no schema in message so assume it was just plain JSON
             return json.loads(data)
         
-        # magic byte is 0 and schema id > 0, so lookup the schema and format and decode data
+        # if this schema id different from the previous one, then setup new schema for decoding
         if self.schema_id != schema_id:
             self.schema_id = schema_id
-            # get all subjects for the schema id
-            subjects = self.schema_registry_client.get_subjects_by_schema_id(self.schema_id)
-            self.subject_name = subjects[0]
-            # get the schema for the latest version of the subject
-            self.schema = self.schema_registry_client.get_latest_version(self.subject_name)
+            self.subject_name = self.get_subject_name(self.schema_id)
+            self.schema = self.get_schema(self.subject_name)
             self.format = self.schema.schema.schema_type
-            # if schema format is AVRO, setup the deserializer
             if self.format == 'AVRO':
+                # if schema format is AVRO, setup the deserializer
                 self.avro_deserializer = AvroDeserializer(
                     schema_registry_client = self.schema_registry_client,
                     schema_str = self.schema.schema.schema_str,
@@ -99,6 +111,6 @@ class Serializer:
                 self.avro_deserializer = None
 
         if self.format == 'AVRO':
-            return self.avro_deserializer(data, SerializationContext(topic, MessageField.VALUE))
+            return self.decode(topic, data)
         else:
             return json.loads(data)
